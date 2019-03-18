13923332087a031e702bf19c50a4ca7e574e3f45
Janile Dantas <janille_dantas@hotmail.com>
2019-03-18 00:54:15 -0300

    requisicao ajax $http

--- js/controllers/fotos-controller.js
+++ js/controllers/fotos-controller.js
@@ -1,18 +1,23 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
-    $scope.fotos = [
-        {
-            titulo : 'Leão',
-            url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-        },
-        {
-            titulo : 'Leão2',
-            url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-        },
-        {
-            titulo : 'Leão3',
-            url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-        },
-    ];
+    $scope.fotos = [];
+
+    $http.get('/v1/fotos')
+    .success(function(retorno) {
+        console.log(retorno);
+        $scope.fotos = retorno; //nao precisa fazer retorno.data
+    })
+    .error(function(erro) {
+        console.log(erro);
+    });
+    
+    /*var promise = $http.get('/v1/fotos');
+    promise.then(function(retorno) {
+        $scope.fotos = retorno.data;
+    })
+    .catch(function(error) {
+        console.log(error)
+    });
+    */
 
 });
