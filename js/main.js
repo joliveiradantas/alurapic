03a5e9ea707c9635478da55e0840d02b73ee262b
Janile Dantas <janille_dantas@hotmail.com>
2019-04-03 21:29:13 -0300

    add $locationProvider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
     //atraves do servico routeProvider que obtenho atraves da injecao, dentro da funcao config
-    .config(function($routeProvider) {
+    .config(function($routeProvider, $locationProvider) {
+
+        $locationProvider.html5Mode(true);
 
         $routeProvider.when('/fotos/', {
 
             //o angular precisa saber que o endereco que estamos tentando acessar nao é um endereco do server
+            //para isso adicionamos o #
+            //o index é carregado mas atraves do sist. de rotas vai buscar a parcial
+            //Com o HTML5 e a History App foi possivel conseguir o mesmo resultado sem o #
+            //Ativo esse servico atraves de $locationProvider que peco atraves do sist. de injecao de dependencias
 
             templateUrl: 'partials/principal.html',
             controller: 'FotosController'
